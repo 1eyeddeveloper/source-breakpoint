605a9ee9559e4fbb34d3eababd476e982f796d02
1eyeddeveloper <1eyeddeveloper@gmail.com>
2022-04-12 04:48:06 -0700

    Added a method to LOGGER to help traverse an object's nested properties

--- breakpoint.js
+++ breakpoint.js
@@ -1,18 +1,30 @@
-(function(){
+(function () {
   const LOGGER = {};
   LOGGER.stringify = function (value) {
-      if (value) {
-        if (value instanceof RegExp) {
-          return value.toString();
-        } else if (value instanceof Function) {
-          return value.toString();
-        } else if (value instanceof Map) {
-          return `new Map(${JSON.stringify([...value.entries()])})`;
-        } else if (value instanceof Array || value instanceof Object) {
-          return JSON.stringify(value);
-        } else {
-          return `${value}`;
-        }
-      } else if (typeof value !== 'undefined') return value + '';/* stringify null values */
-    }
+    if (value) {
+      if (value instanceof RegExp) {
+        return value.toString();
+      } else if (value instanceof Function) {
+        return value.toString();
+      } else if (value instanceof Map) {
+        return `new Map(${JSON.stringify([...value.entries()])})`;
+      } else if (value instanceof Array || value instanceof Object) {
+        return JSON.stringify(value);
+      } else {
+        return `${value}`;
+      }
+    } else if (typeof value !== 'undefined') return value + '';/* stringify null values */
+  }
+
+  /* getnestedobj,  a function to which fetches a deeply nested property of an object, when passed the object(parentobj) and a string format of property hierarchy(nestedkey) to the target property with format: '[prop1][prop2][prop3]'  */
+  LOGGER.getnestedobj = function (parentobj, nestedkey) {
+    if (typeof nestedkey == 'undefined') return 'report complete!';
+    if (nestedkey === '') return parentobj;
+    let obj;
+    nestedkey.slice(1).replace(/\]/ig, '').split('[').forEach(x => {
+      if (!obj) { obj = parentobj[x]; return; };
+      obj = obj[x];
+    })
+    return obj;
+  }
 })();
